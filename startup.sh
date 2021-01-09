79b23a098137afcdefd5f4e733f8b5f7a8aaa4bb
Chirurgus <sasha.sashaxl@gmail.com>
2021-01-09 22:29:28 +0100

    Actually do what last commit said

--- startup.sh
+++ startup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 source /opt/mycroft/.venv/bin/activate
+
+# Force skills to re-install dependnecies
+rm $HOME/.mycroft/.mycroft-skill
+rm /opt/mycroft/skills/.msm
+
+# Run mycroft
 /opt/mycroft/./start-mycroft.sh all
 
 tail -f /var/log/mycroft/*.log
